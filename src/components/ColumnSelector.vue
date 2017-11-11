f76ad7e3a6c1c3e2440a049ca4864ddc2d82ae46
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-11-11 23:34:32 +0100

    Prettify column selector a bit.

--- src/components/ColumnSelector.vue
+++ src/components/ColumnSelector.vue
@@ -1,43 +1,44 @@
 <template>
 <div class="column-selector builder-box">
   <h5 class="title is-5">Result columns</h5>
-  <div class="field is-grouped is-grouped-multiline">
+  <div class="field is-grouped">
     <p class="control">
-      <label class="button checkbox is-small">
+      <label class="button checkbox">
         <input type="checkbox" checked>
         item id
       </label>
     </p>
 
     <p class="control">
-      <label class="button checkbox is-small">
+      <label class="button checkbox">
         <input type="checkbox" checked>
         item label
       </label>
     </p>
+  </div>
 
-    <div class="field is-small">
-      <div class="control is-small">
-        <MultiSelect
-          :options="properties"
-          v-model="selectedProperties"
-          :searchable="true"
-          :multiple="true"
-          :internal-search="false"
-          @search-change="getOptions"
-          @input="updateSelectedColumns">
-          <template slot="option" scope="props">
-            <div class="option__desc">
-              <span class="option__title">{{ props.option.label }}</span>
-              <span class="option__small">{{ props.option.description }}</span>
-            </div>
-          </template>
+  <div class="field">
+    <div class="control">
+      <MultiSelect
+        :options="properties"
+        v-model="selectedProperties"
+        :multiple="true"
+        :close-on-select="false"
+        :internal-search="false"
+        placeholder="Select properties"
+        @search-change="getOptions"
+        @input="updateSelectedColumns">
+        <template slot="option" scope="props">
+          <span class="dropdown-item-label">{{ props.option.label }}</span>
+          <span class="dropdown-item-description">{{ props.option.description }}</span>
+        </template>
 
-          <template slot="tag" scope="props">
+        <template slot="tag" scope="props">
+          <span class="selected">
             {{ props.option.label }}
-          </template>
-        </MultiSelect>
-      </div>
+          </span>
+        </template>
+      </MultiSelect>
     </div>
   </div>
 </div>
@@ -112,6 +113,27 @@ export default {
   margin-top: 10px;
 
   .multiselect {
+    .multiselect__tags {
+      border-radius: $input-radius;
+      border-color: $input-border-color;
+      box-shadow: $input-shadow;
+
+      input {
+        margin-bottom: 0;
+      }
+    }
+
+    .multiselect__option--highlight {
+      background: $primary;
+    }
+
+    .selected {
+      border-radius: 2px;
+      padding: 2px;
+      margin: -2px 2px 0 0;
+      display: inline-block;
+      border: 1px solid $grey-lighter;
+    }
   }
 }
 </style>
