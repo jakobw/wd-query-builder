f7811f73d179ba5e3420b0fc9292b69c9c02934b
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-11-25 15:56:47 +0100

    Add subclass of subclass note.

--- src/queryBuilder/specialProperties.js
+++ src/queryBuilder/specialProperties.js
@@ -4,5 +4,11 @@ export default [
     longDescription: '(e.g. include instances of "painting" when filtering for instance of "work of art")',
     triggerId: 'P31',
     specialId: 'P31/wdt:P279*'
-  }
+  },
+  {
+    description: 'include subclasses of subclasses',
+    longDescription: '(e.g. when filtering for "animal" include "cat" which is a subclass of "domesticated animal")',
+    triggerId: 'P279',
+    specialId: 'P279|wdt:P279*'
+  },
 ]
