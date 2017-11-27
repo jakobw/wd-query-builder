3f77dec3690a4e89893ab322534c44e5b5bbcf47
Jakob Warkotsch <j.warkotsch@gmail.com>
2017-11-27 15:44:00 +0100

    Add share modal.

--- src/components/ShareQuery.vue
+++ src/components/ShareQuery.vue
@@ -1,16 +1,37 @@
 <template>
 <div class="share-query">
   <button class="button" @click="share">Share Query</button>
+
+  <div class="modal" :class="{ 'is-active': isActive }">
+    <div class="modal-background" @click="isActive = false"></div>
+    <div class="modal-content">
+      <div class="box">
+        <h5 class="title is-5">You can share your query with the link below</h5>
+        <input class="input" v-model="link">
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="isActive = false"></button>
+  </div>
 </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import QuerySerializer from '../serialization/QuerySerializer'
 
 export default {
+  data() {
+    return {
+      link: '',
+      isActive: false
+    }
+  },
+
   methods: {
     share() {
-      console.log(this.generateShareUrl())
+      this.link = this.generateShareUrl()
+      this.isActive = true
+      Vue.nextTick(() => this.$el.querySelector('input').select())
     },
 
     generateShareUrl() {
